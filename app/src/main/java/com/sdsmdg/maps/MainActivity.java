771dddadcc03bf9d657ff855ff2d78e1d94bdc3b
Rahul Yadav <rahuly247@gmail.com>
2016-11-23 14:22:38 +0530

    Initial Commit

---
+++ app/src/main/java/com/sdsmdg/maps/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sdsmdg.maps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static android.R.attr.value;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
