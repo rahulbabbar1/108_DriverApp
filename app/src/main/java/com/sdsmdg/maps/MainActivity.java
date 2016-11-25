6fbc9438c233759b5a97c279c1ca7f7ce03fccf4
Rahul Babbar <rahulbabbar1@live.com>
2016-11-25 19:06:36 +0530

    Added location fetcher

--- app/src/main/java/com/sdsmdg/maps/MainActivity.java
+++ app/src/main/java/com/sdsmdg/maps/MainActivity.java
@@ -1,10 +1,16 @@
 package com.sdsmdg.maps;
 
+import android.*;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -34,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     MarkerOptions clientPosition;
     boolean mapReady = false;
     boolean shouldShow = false;
+    public Activity mainActivity;
+    public String TAG="MainActivity";
 
     //TODO Convert int to float
     @Override
@@ -43,6 +51,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         show = (Button) findViewById(R.id.show);
         lat = (EditText) findViewById(R.id.lat);
         lng = (EditText) findViewById(R.id.lng);
+
+        mainActivity=MainActivity.this;
+        myCheckPermission(mainActivity);
+        Intent i= new Intent(MainActivity.this,GetLocation.class);
+        startService(i);
 
 
 
@@ -135,6 +148,76 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
+    public void myCheckPermission(Activity thisActivity){
+        // Here, thisActivity is the current activity
+        if( ContextCompat.checkSelfPermission(thisActivity,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED||(ContextCompat.checkSelfPermission(thisActivity,
+                android.Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED)||(ContextCompat.checkSelfPermission(thisActivity,
+                android.Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED)||(ContextCompat.checkSelfPermission(thisActivity,
+                android.Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED) ){
+
+//            // Should we show an explanation?
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity,
+//                    Manifest.permission.READ_CONTACTS)) {
+//
+//                // Show an explanation to the user *asynchronously* -- don't block
+//                // this thread waiting for the user's response! After the user
+//                // sees the explanation, try again to request the permission.
+//
+//            } else {
+
+            // No explanation needed, we can request the permission.
+            Log.d(TAG, "myCheckPermission() if called with: " + "thisActivity = [" + thisActivity + "]");
+
+            ActivityCompat.requestPermissions(thisActivity,
+                    new String[]{ android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.INTERNET, android.Manifest.permission.SEND_SMS, android.Manifest.permission.RECEIVE_SMS},
+                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+            //}
+
+        }
+        else{
+            Log.d(TAG, "myCheckPermission() else called with: " + "thisActivity = [" + thisActivity + "]");
+            Intent i= new Intent(MainActivity.this,GetLocation.class);
+            startService(i);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                myCheckPermission(mainActivity);
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
 }
